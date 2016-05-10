429fcaf4e2705d2576e0ba1552c24215eea2afb6
Fran <ftarulla@gmail.com>
2016-05-10 20:24:58 -0300

    New arrow samples

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# playingWithES6
+# Playing with ECMAScript 6
 
 Just playing with ECMAScript 6 ... just for fun!! :)
+
+Compiling with Babel
+====================
+
+1. npm run build
+2. node /bin/<file.js>
--- src/arrow.js
+++ src/arrow.js
@@ -1,3 +1,12 @@
-var Add1 = x => x + 1;
+var add1 = x => x + 1;
 
-console.log(Add1(1));
+console.log(add1(1)); // 2
+
+var allplus1 = [1,2,3].map(e => e + 1);
+console.log(allplus1);  // 2,3,4
+
+var allplus1_v2 = [1,2,3].map(add1);
+console.log(allplus1_v2); // 2,3,4
+
+var addn = n => (x => x + n);
+console.log([1,2,3].map(addn(10))); //11,12,13
