8979af096c57b73aa5c1db3e4285b78ff12c4aa4
Fran <ftarulla@gmail.com>
2016-05-10 22:36:43 -0300

    Arrow binding THIS

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Just playing with ECMAScript 6 ... just for fun!! :)
 
 Compiling with Babel
-====================
+--------------------
 
 1. npm run build
-2. node /bin/<file.js>
+2. node /bin/*file.js*
--- bin/arrow.js
+++ bin/arrow.js
@@ -5,56 +5,54 @@ var add1 = x => x + 1;
 
 console.log(add1(1)); // 2
 
-console.log([1,2,3].map(e => e + 1));  // 2,3,4
+console.log([1, 2, 3].map(e => e + 1)); // 2,3,4
 
-console.log([1,2,3].map(add1)); // 2,3,4
+console.log([1, 2, 3].map(add1)); // 2,3,4
 
-var addn = n => (x => x + n);
-console.log([1,2,3].map(addn(10))); //11,12,13
+var addn = n => x => x + n;
+console.log([1, 2, 3].map(addn(10))); //11,12,13
 
-
-// Not Just Shorter Syntax, But this
-// https://github.com/getify/You-Dont-Know-JS/blob/master/es6%20&%20beyond/ch2.md#not-just-shorter-syntax-but-this
-var Controller = function() {
+// => is a syntactic stand-in for var self = this.
+var Controller = function () {
   return {
     foo: 'bar',
-    bla: function() {
-      (function() {
+    bla: function () {
+      (function () {
         console.log(this); // global
         console.log(this.foo);
       })();
     }
-  }
+  };
 };
 var controller = new Controller();
 controller.bla(); // undefined
 
-var ControllerFix = function() {
+var ControllerFix = function () {
   return {
     foo: 'bar',
-    bla: function() {
-      var self = this;  // Binding THIS
-      (function() {
+    bla: function () {
+      var self = this;
+      (function () {
         console.log(self); // {foo: 'bar', bla: [Function]}
         console.log(self.foo);
       })();
     }
-  }
+  };
 };
 var controllerFix = new ControllerFix();
 controllerFix.bla(); // undefined
 
-// => is a syntactic stand-in for var self = this.
-var Controller2 = function() {
+var Controller2 = function () {
   return {
     foo: 'bar',
-    bla: function() {
-      (() => { //Lexical this in the arrow function
+    bla: function () {
+      (() => {
+        //Lexical this in the arrow function
         console.log(this); // {foo: 'bar', bla: [Function]}
         console.log(this.foo);
       })();
     }
-  }
+  };
 };
 var controller2 = new Controller2();
 controller2.bla(); // 'bar'
